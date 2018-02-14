0078e9a88a7ec40346614431d8312df004022cfd
Eric Fuller <info@glassmoonmedia.com>
2018-02-13 20:18:37 -0500

    Add in some initial state along with package installation.

---
+++ src/store/initialState.js
@@ -0,0 +1,13 @@
+const initialState = {
+    users: [
+        {
+            id: 1,
+            firstName: 'Eric',
+            lastNamme: 'Fuller',
+            email: 'info@glassmoonmedia.com',
+            password: 'password',
+        }
+    ]
+};
+
+export default initialState;
